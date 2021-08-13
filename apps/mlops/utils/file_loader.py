53066dad0de922d7c91afe2ac0c8cb7574a7ae60
Rocuant Roberto <atrysrocuant@MacBook-Pro-de-Rocuant.local>
2021-08-13 05:27:55 -0400

    v0.7.0 catsvsdogs ready, floor for output, argmax/threshold output fixed

--- apps/mlops/utils/file_loader.py
+++ apps/mlops/utils/file_loader.py
@@ -1,12 +1,12 @@
 import os
 
-from django.conf import settings
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from PIL import (
     Image,
     ImageFile,
 )
+from main.settings import MEDIA_ROOT
 from tensorflow import keras
 from typing import (
     Generic,
@@ -54,8 +54,8 @@ class StackoverflowFileLoader:
 
 class CatsvsdogsFileLoader:
 
-    def __call__(self, model_input: Generic[InMemoryUploadedFile]) -> Generic[PILImage]:
+    def __call__(self, model_input: Generic[InMemoryUploadedFile]) -> str:
         path = default_storage.save(f"tmp/{model_input.name}", ContentFile(model_input.file.read()))
         model_input.file.seek(0)
-        model_input = os.path.join(settings.MEDIA_ROOT, path)
+        model_input = os.path.join(MEDIA_ROOT, path)
         return model_input
