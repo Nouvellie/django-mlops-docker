a29234f09b150d6a4a5302acd475eb49c8355933
Rocuant Roberto <atrysrocuant@MacBook-Pro-de-Rocuant.local>
2021-08-13 06:07:09 -0400

    v0.7.0 catsvsdogs ready, floor for output, argmax/threshold output fixed

--- apps/mlops/utils/preprocessing.py
+++ apps/mlops/utils/preprocessing.py
@@ -11,18 +11,18 @@ from typing import (
 
 
 @pipeline_function_register
-def resize_img(model_input: Any, target_size: Tuple = (28, 28)) -> Any:
+def resize_img(model_input: Any, target_size: Tuple = (28, 28), *args, **kwargs) -> Any:
     return model_input.resize(target_size)
 
 
 @pipeline_function_register
-def rescale_img(model_input: Any, factor: float = 255.0) -> Any:
+def rescale_img(model_input: Any, factor: float = 255.0, *args, **kwargs) -> Any:
     """Scale these values to a range of 0 to 1 before feeding them to the neural network model."""
     return np.array(model_input)/factor
 
 
 @pipeline_function_register
-def img_to_array(model_input: Any) -> Any:
+def img_to_array(model_input: Any, *args, **kwargs) -> Any:
     model_input = keras.preprocessing.image.img_to_array(model_input)
     return model_input
 
@@ -31,7 +31,7 @@ def img_to_array(model_input: Any) -> Any:
 def load_img(model_input: Any, target_size: Tuple = (180, 180)) -> Any:
     path_to_remove = str(model_input)
     model_input = keras.preprocessing.image.load_img(
-        model_input, target_size=(180,180)
+        model_input, target_size=target_size
     )
     os.remove(path_to_remove)
     return model_input
