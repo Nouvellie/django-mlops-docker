96d93c6ac014b3b2e24fa8e7d652dcb764432da6
Rocuant Roberto <atrysrocuant@MacBook-Pro-de-Rocuant.local>
2021-08-18 18:42:02 -0400

    v0.14.0 docker full

--- src/main/main/settings.py
+++ src/main/main/settings.py
@@ -110,8 +110,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Without docker.
-# STATIC_ROOT = '/static/' # With docker.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
